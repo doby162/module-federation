415f054a69a2eacf446ca1f70a9d185b641e0c4e
Michael Dorian <michael@ulap.digital>
2025-10-01 10:07:56 -0500

    update react and gitignore modules

--- provider/src/components/Button.tsx
+++ provider/src/components/Button.tsx
@@ -4,7 +4,6 @@ const Button: React.FC = () => {
   const [count, setCount] = React.useState(0)
   return (
     <div >
-      <p>this is the second button</p>
       <button onClick={()=>{setCount(count+1)}}>{count}</button>
     </div>
 
